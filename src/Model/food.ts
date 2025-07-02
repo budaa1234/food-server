041293f3692f5c9e9abf59973a8020c7f4bd3c92
25LP4381 25LP4381 <25LP4381@pinecones-iMac.local>
2025-07-02 12:58:35 +0800

    df

--- src/Model/food.ts
+++ src/Model/food.ts
@@ -2,7 +2,7 @@ import { model, Schema } from "mongoose";
 
 const foodSchema = new Schema({
   foodName: { type: String, required: true },
-  price: { type: String, required: true },
+  price: { type: Number, required: true },
   image: { type: String, required: true },
   ingredients: { type: String, required: true },
   category: {
