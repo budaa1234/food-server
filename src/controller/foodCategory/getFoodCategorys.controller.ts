96e133e9e0376ef70c609cabb21f1d0588c5fe8b
25LP4381 25LP4381 <25LP4381@G303-CC0138.local>
2025-07-03 18:42:18 +0800

    dd

--- src/controller/foodCategory/getFoodCategorys.controller.ts
+++ src/controller/foodCategory/getFoodCategorys.controller.ts
@@ -3,7 +3,26 @@ import FoodCategory from "../../Model/foodCategory";
 
 export const getFoodCategories = async (req: Request, res: Response) => {
   try {
-    const categories = await FoodCategory.find();
+    const categories = await FoodCategory.aggregate([
+      {
+        $lookup: {
+          from: "foods",
+          localField: "_id",
+          foreignField: "category",
+          as: "categoryDetails",
+        },
+      },
+      {
+        $project: {
+          categoryName: "$categoryName",
+          count: { $size: "$categoryDetails" },
+          foods: "$categoryDetails",
+        },
+      },
+      {
+        $sort: { categoryName: 1 },
+      },
+    ]);
     res.status(200).send({ succses: true, categories });
   } catch (error) {
     res.status(400).send({ message: "api error", error });
