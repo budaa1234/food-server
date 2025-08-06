49c47d1b76864dbbea16f3f126f7b6db524c96ea
25LP4381 25LP4381 <25LP4381@pinecones-iMac.local>
2025-08-06 08:36:50 +0800

    123

--- src/controller/auth/getCurrentUser.controller.ts
+++ src/controller/auth/getCurrentUser.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import jwt, { verify } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import User from "../../Model/user";
 
 
 export const getCurrentUser = async (req: Request, res: Response) => {
@@ -11,12 +12,16 @@ export const getCurrentUser = async (req: Request, res: Response) => {
    }
     
     try {
-        const verified = jwt.verify(token!, process.env.JSONWEBTOKEN!);
-        console.log(verified);
+        const verified = jwt.verify(token!, process.env.JSONWEBTOKEN!) as jwt.JwtPayload;
+        
         if(!verified) {
             throw new Error("User not found");
         }
-        res.status(200).json({user: verified});
+
+        const user = await User.findOne({_id: verified.data.userId});
+       
+        
+        res.status(200).json({user: user});
     } catch (error) {
         
     }
